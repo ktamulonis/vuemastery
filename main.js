0bbe4603ff86f13471e66717c30985e77f4b391e
ktamulonis <kurttamulonis@gmail.com>
2019-02-23 14:30:13 -0500

    refactored computed property title from brand and product display title instead of product

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app', 
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
     image: './assets/vmSocks-green.jpg',
     inStock: true,
@@ -32,5 +33,10 @@ var app = new Vue({
     updateProduct(variantImage) {
       this.image = variantImage
     }
-  }
+  },
+    computed: {     
+      title() {
+        return this.brand + ' ' + this.product
+      }  
+   }
 }) 
