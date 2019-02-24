d3021ca077f3a85ffedcc1da776ae1acfca4f770
ktamulonis <kurttamulonis@gmail.com>
2019-02-24 13:34:55 -0500

    passes premium prop into component template to display

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
   template: `
     <div class="product">
 
@@ -11,7 +17,7 @@ Vue.component('product', {
         <p v-show="inStock">In Stock</p>
         <p v-show="!inStock" class='line-through'>Out of Stock</p>
         <p v-if="onSale">On Sale!</p>
-
+        <p>User is premium: {{ premium }}</p>
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
@@ -96,5 +102,7 @@ Vue.component('product', {
 
 var app = new Vue({
   el: '#app', 
-  
+  data: {
+    premium: true
+  }  
 }) 
